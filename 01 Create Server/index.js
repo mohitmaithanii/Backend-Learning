1b89c768993922bbeb26b8a169e68cb4b82d2e98
Mohit Maithani <126086791+mohitmaithanii@users.noreply.github.com>
2024-04-19 21:38:53 +0530

    "Add routes for root, twitter, login, and youtube; start server on specified port"
    "add Connect frontend and backend folder"

--- 01 Create Server/index.js
+++ 01 Create Server/index.js
@@ -1,25 +1,34 @@
+// Import the required modules
 require("dotenv").config();
-
 const express = require("express");
+
+// Create an instance of express
 const app = express();
+
+// Set the port number
 const port = 3000;
 
+// Define a route for the root URL ("/") and send "Hello World!" as the response
 app.get("/", (req, res) => {
 	res.send("Hello World!");
 });
 
+// Define a route for the "/twitter" URL and send "mohitmaithaniii" as the response
 app.get("/twitter", (req, res) => {
 	res.send("mohitmaithaniii");
 });
 
+// Define a route for the "/login" URL and send a login prompt as the response
 app.get("/login", (req, res) => {
 	res.send("<h1> please login at chai aur code</h1>");
 });
 
+// Define a route for the "/youtube" URL and send "Chai aur code" as the response
 app.get("/youtube", (req, res) => {
 	res.send("Chai aur code");
 });
 
+// Start the server and listen on the specified port
 app.listen(process.env.PORT, () => {
 	console.log(`Example app listening on port ${port}`);
 });
