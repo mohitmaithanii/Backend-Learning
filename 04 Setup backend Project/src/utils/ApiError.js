7acd7ffb765506d1679d264188d2a4f25b7defa4
Mohit Maithani <126086791+mohitmaithanii@users.noreply.github.com>
2024-04-23 05:56:05 +0530

    Add cloudinary and multer dependencies for  file uploading

--- 04 Setup backend Project/src/utils/ApiError.js
+++ 04 Setup backend Project/src/utils/ApiError.js
@@ -1,26 +1,27 @@
 // Custom error class for API errors
 class ApiError extends Error {
-  // Constructor takes in statusCode, message, stack, and errors as parameters
-  constructor(
-    statusCode,
-    message = "Something went wrong",
-    stack = "",
-    errors = []
-  ) {
-    // Call the parent constructor with all parameter
-    super(message);
-    this.statusCode = statusCode;
-    this.data = null;
-    this.success = false;
-    this.errors = errors;
+    // Constructor takes in statusCode, message, stack, and errors as parameters
+    constructor(
+        statusCode,
+        message = "Something went wrong",
+        stack = "",
+        errors = []
+    ) {
+        // Call the parent constructor with all parameter
+        super(message);
+        this.statusCode = statusCode;
+        this.data = null;
+        this.message = message;
+        this.success = false;
+        this.errors = errors;
 
-    // If the stack parameter is provided, set the stack property to it Otherwise, capture the stack trace using Error.captureStackTrace
-    if (stack) {
-      this.stack = stack;
-    } else {
-      Error.captureStackTrace(this, this.constructor);
+        // If the stack parameter is provided, set the stack property to it Otherwise, capture the stack trace using Error.captureStackTrace
+        if (stack) {
+            this.stack = stack;
+        } else {
+            Error.captureStackTrace(this, this.constructor);
+        }
     }
-  }
 }
 
 export { ApiError };
