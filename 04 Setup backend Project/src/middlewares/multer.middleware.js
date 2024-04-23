9b439cfd81ab8c6bb3382a5feccb3696caf6c4c1
Mohit Maithani <126086791+mohitmaithanii@users.noreply.github.com>
2024-04-23 17:34:50 +0530

    add some comments and notes

--- 04 Setup backend Project/src/middlewares/multer.middleware.js
+++ 04 Setup backend Project/src/middlewares/multer.middleware.js
@@ -1,12 +1,17 @@
+// importing multer module
 import multer from "multer";
 
+// configuring multer storage object
 const storage = multer.diskStorage({
+    // destination function to specify where the uploaded files will be stored
     destination: function (req, file, cb) {
-        cb(null, "./public/temp");
+        cb(null, "./public/temp"); // specifying the destination folder
     },
+    // filename function to specify the name of the uploaded files
     filename: function (req, file, cb) {
-        cb(null, file.originalname);
+        cb(null, file.originalname); // using the original file name
     },
 });
 
+// exporting the multer object with the specified storage configuration
 export const upload = multer({ storage });
--- 04 Setup backend Project/src/utils/couldinary.js
+++ 04 Setup backend Project/src/utils/couldinary.js
@@ -1,27 +1,37 @@
 import { v2 as cloudinary } from "cloudinary";
 import fs from "fs";
 
+// configuring cloudinary
 cloudinary.config({
     cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
     api_key: process.env.CLOUDINARY_API_KEY,
     api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
+// defining a function to upload a file to cloudinary
 const uploadOnCloudinary = async (localFilePath) => {
     try {
+        // returning null if no file path is provided
         if (!localFilePath) return null;
 
-        //upload the file on cloudinary
+        // uploading the file to cloudinary
         const response = await cloudinary.uploader.upload(localFilePath, {
             resource_type: "auto",
         });
-        // file has been uploaded successful
+
+        // logging the url of the uploaded file
         console.log("File is uploaded on cloudinary ", response.url);
+
+        // returning the response
         return response;
     } catch (error) {
-        fs.unlinkSync(localFilePath); // remove the locally saved temporary file as the upload operation got failed
+        // removing the locally saved temporary file if the upload operation failed
+        fs.unlinkSync(localFilePath);
+
+        // returning null
         return null;
     }
 };
 
+// exporting the uploadOnCloudinary function
 export { uploadOnCloudinary };
