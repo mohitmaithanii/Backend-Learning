ff45e3363897ba987c08971a9969c5a528736576
Mohit Maithani <126086791+mohitmaithanii@users.noreply.github.com>
2024-04-24 04:21:44 +0530

    add comments

--- 04 Setup backend Project/src/controllers/user.controller.js
+++ 04 Setup backend Project/src/controllers/user.controller.js
@@ -1,7 +1,8 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
 
 const registerUser = asyncHandler(async (req, res) => {
-    res.status(200).json({ message: "OK !!" });
+    // sending a response with status code 200 and a message
+    res.status(200).json({ message: "OK!!" });
 });
 
 export { registerUser };
--- 04 Setup backend Project/src/routes/user.routes.js
+++ 04 Setup backend Project/src/routes/user.routes.js
@@ -3,6 +3,7 @@ import { registerUser } from "../controllers/user.controller.js";
 
 const router = Router();
 
+// defining a route for the /register endpoint
 router.route("/register").post(registerUser);
 
 export default router;
